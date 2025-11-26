65743f4dbda4a32e83d1e07c445f8f8b5481775f
Azhar786786786 <talkazhar786@hotmail.com>
2025-11-26 20:44:34 +0530

    Added Polymorphism concept

---
+++ SOLID_With_DotNet/AllClasses/PolymorphismClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_With_DotNet.AllClasses
+{
+    class PolymorphismClass
+    {
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime InsertedOn { get; set; }
+        public virtual void DoWokr()
+        {
+            Console.WriteLine("Person work running");
+        }
+    }
+    class Engineer : Person
+    {
+        public override void DoWokr()
+        {
+            base.DoWokr();
+        }
+    }
+
+    class Tester : Person
+    {
+        public override void DoWokr()
+        {
+            //base.DoWokr();
+            Console.WriteLine("Tester work running");
+        }
+    }
+
+    class Other : Person
+    {
+        public new void DoWokr()
+        {
+            //base.DoWokr();
+            Console.WriteLine("Other work running");
+        }
+    }
+}
--- SOLID_With_DotNet/Program.cs
+++ SOLID_With_DotNet/Program.cs
@@ -27,6 +27,23 @@ namespace SOLID_With_DotNet
             notifier = new EmailNotifier();
             notifier.Notify();
 
+            //Checking for polymorphism concept
+            //PolymorphismClass polymorphismClass = new PolymorphismClass();
+            //polymorphismClass.
+            Console.WriteLine("============================================================");
+            Person person = new Person();
+            person.DoWokr();
+
+            Person eng = new Engineer();
+            eng.DoWokr();
+
+            Person test = new Tester();
+            test.DoWokr();
+
+            Person oth = new Other();
+            oth.DoWokr();
+            //Polymorphism end here
+
             Console.Read();
         }
     }
