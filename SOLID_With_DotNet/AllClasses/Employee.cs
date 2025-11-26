0aeb6956073346cd155ecdcae58b1999352968f2
Azhar786786786 <talkazhar786@hotmail.com>
2025-11-26 10:09:45 +0530

    Add project files.

---
+++ SOLID_With_DotNet/AllClasses/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_With_DotNet.AllClasses
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public void Update()
+        {
+            //Employee Details
+            Console.WriteLine("Updating employee with ID: ", this.ID);
+        }
+    }
+    public class EmployeeSalaryCalculator
+    {
+        public void CalculateSalary()
+        {
+            //Hours/Role/Bonus
+            //Console.WriteLine("Calculating salary for employee: ", Employee.Name);
+        }
+    }
+    public abstract class Notifier
+    {
+        public abstract void Notify();
+    }
+    public class EmailNotifier : Notifier
+    {
+        public override void Notify()
+        {
+            //Email/SMS/Letter
+            Console.WriteLine("Notification Send with Email successfully");
+        }
+    }
+    public class SMSNotifier : Notifier
+    {
+        public override void Notify()
+        {
+            //Email/SMS/Letter
+            Console.WriteLine("Notification Send with SMS successfully");
+        }
+    }
+    public class LetterNotifier : Notifier
+    {
+        public override void Notify()
+        {
+            //Email/SMS/Letter
+            Console.WriteLine("Notification Send with Letter successfully");
+        }
+    }
+    public class PhoneNotifier : Notifier
+    {
+        public override void Notify()
+        {
+            //Email/SMS/Letter
+            Console.WriteLine("Notification Send with Phone successfully");
+        }
+    }
+}
---
+++ SOLID_With_DotNet/Program.cs
@@ -0,0 +1,33 @@
+﻿using SOLID_With_DotNet.AllClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_With_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.ID = 101;
+            employee.Name = "Added NewEmplyee";
+
+            Notifier notifier = new SMSNotifier();
+            notifier.Notify();
+
+            notifier = new LetterNotifier();
+            notifier.Notify();
+
+            notifier = new PhoneNotifier();
+            notifier.Notify();
+
+            notifier = new EmailNotifier();
+            notifier.Notify();
+
+            Console.Read();
+        }
+    }
+}
